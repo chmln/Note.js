d69d5db191dad746ddc0d4f06d58c9044318ba3d
Gregory <gregory.petrosyan@mail.utoronto.ca>
2016-11-07 01:01:15 -0500

    implement 4-corner positioning options

--- src/note.js
+++ src/note.js
@@ -12,10 +12,14 @@ const Note = function (config) {
     }
 
     function init() {
+        parseConfig();
         build();
+    }
 
+    function parseConfig() {
         self.config = {
             duration: 4,
+            position: "topRight",
             closeIcon: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="17" height="17" viewBox="0 0 17 17"> <g> </g> <path d="M9.207 8.5l6.646 6.646-0.707 0.707-6.646-6.646-6.646 6.646-0.707-0.707 6.646-6.646-6.647-6.646 0.707-0.707 6.647 6.646 6.646-6.646 0.707 0.707-6.646 6.646z" /></svg>'
         };
 
@@ -25,7 +29,9 @@ const Note = function (config) {
     }
 
     function build() {
-        self.container = createElement("div", "note--container");
+        self.container = createElement("div", "note--container " + self.config.position);
+        self.innerContainer = createElement("div", "note--inner");
+        self.container.appendChild(self.innerContainer);
         document.body.appendChild(self.container);
     }
 
@@ -33,14 +39,14 @@ const Note = function (config) {
         document.body.removeChild(self.container);
     }
 
-    function showGeneric(type, title, text, config) {
+    function showGeneric(type, title, text, noteConfig) {
         // {
         //     type: ["info", "success", "error", "warning"],
         //     title: "",
         //     text: ""
         // }
 
-        config = config || {};
+        noteConfig = noteConfig || {};
 
         let note = createElement("div", "note shown note--" + type);
         let noteFragment = document.createDocumentFragment(),
@@ -54,9 +60,9 @@ const Note = function (config) {
         noteFragment.appendChild(createElement("p", "note--body", text));
 
         note.appendChild(noteFragment);
-        self.container.appendChild(note);
+        self.innerContainer.appendChild(note);
 
-        let onAnimationEnd = e => !note.classList.contains("shown") && (self.container.removeChild(note));
+        let onAnimationEnd = e => !note.classList.contains("shown") && (self.innerContainer.removeChild(note));
         let hide = () => {
             note.classList.remove("shown");
         };
@@ -64,17 +70,17 @@ const Note = function (config) {
         noteCloseButton.addEventListener("click", hide);
         note.addEventListener("animationend", onAnimationEnd);
 
-        if (!config.stick && !self.config.stick)
+        if (!noteConfig.stick && !self.config.stick)
             setTimeout(function(){
-                self.container.removeChild(note);
-            }, (config.duration || self.config.duration) * 1000);
+                self.innerContainer.removeChild(note);
+            }, (noteConfig.duration || self.config.duration) * 1000);
 
         return {
             hide: hide,
 
             show: function (){
                 if (!note.parentNode)
-                    self.container.appendChild(note);
+                    self.innerContainer.appendChild(note);
                 note.classList.add("shown");
             }
         }

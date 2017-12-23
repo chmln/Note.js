73f9862f18efb643eb1bb7c67285cca186d4cde1
Qathom <tony9295@gmail.com>
2017-12-23 17:50:13 +0100

    Fix conditional rule

--- src/note.js
+++ src/note.js
@@ -29,9 +29,10 @@ const Note = function (config) {
 			closeIcon: "<svg viewbox=\"0 0 40 40\"><path d=\"M 10,10 L 30,30 M 30,10 L 10,30\" /></svg>"
 		};
 
-		if (config)
+		if (config) {
 			for(let opt in config)
 				self.config[opt] = config[opt];
+		}
 	}
 
 	function build() {
